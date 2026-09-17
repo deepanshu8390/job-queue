71376be5615a1f96df5ef99deced7fda2ccd6964
deepanshu <deepanshu8390@gmail.com>
2026-09-17 16:31:58 +0530

    cors

--- backend/src/main.js
+++ backend/src/main.js
@@ -6,7 +6,7 @@ const { ApiErrorFilter } = require('./common/api-error.filter');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: process.env.FRONTEND_ORIGIN || 'http://localhost:5173' });
+  app.enableCors();
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, transform: true, transformOptions: { enableImplicitConversion: true } }));
   app.useGlobalFilters(new ApiErrorFilter());
